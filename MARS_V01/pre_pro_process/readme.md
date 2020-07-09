a93c5e941f8d12ee0f6600cb1fa64af2a5ed9f67
XinshaoAmosWang <xinshaowang@gmail.com>
2020-07-09 13:21:58 +0100

    Code is under legal check...

--- MARS_V01/pre_pro_process/readme.md
+++
@@ -1,1 +0,0 @@
-## Update get_train_minibatch_Mars_V001.m to V002
--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git remote set-url origin git@github.com:XinshaoAmosWang/OSM_CAA_WeightedContrastiveLoss.git
 
-git add *
+git add .
 git commit -m "Code is under legal check..."
 git push origin master
